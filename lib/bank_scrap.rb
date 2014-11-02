61a82fbc20bb39c9b842c49bb5bc21590c928ebb
Javier Cuevas <javi@diacode.com>
2014-11-02 11:05:27 +0100

    Use dynamic loading for banks

--- lib/bank_scrap.rb
+++ lib/bank_scrap.rb
@@ -2,9 +2,9 @@ require 'bank_scrap/version'
 require 'bank_scrap/cli'
 require 'bank_scrap/bank'
 
-# TODO: load banks dynamically
-require 'bank_scrap/banks/bankinter'
-require 'bank_scrap/banks/bbva'
-
 module BankScrap
+  # autoload only requires the file when the specified
+  # constant is used for the first time
+  autoload :Bankinter,  'bank_scrap/banks/bankinter'
+  autoload :Bbva,       'bank_scrap/banks/bbva'
 end
